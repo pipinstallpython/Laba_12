215aec6968b0292a98c6a7f05c0820d60f06167e
Gamid <dim.u7kin@yandex.ru>
2022-12-24 01:59:54 +0300

    adds

--- Pycharm/ind.py
+++ Pycharm/ind.py
@@ -5,9 +5,18 @@ def func(x, n):
     if n == 0:
         return 1
     elif n < 0:
-        return 1 / pow(x, abs(n))
+        return 1 / power(x, abs(n))
     elif n > 0:
-        return x * pow(x, n - 1)
+        return x * power(x, n - 1)
+
+
+def power(a, n):
+    if n == 0:
+        return 1
+    if n % 2 == 0:
+        return power(a, n / 2) ** 2
+    else:
+        return a * power(a, n - 1)
 
 
 if __name__ == '__main__':
